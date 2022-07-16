4dc6835da61040319a96237abc49a0238b790883
Myungho Choi <mchoi@lgcns.com>
2022-07-16 15:49:47 +0900

    table-update

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
+import { Button, Table, Container, Col, Row, Image } from 'react-bootstrap';
+
 
 function Detail() {
   
@@ -32,7 +34,80 @@ function Detail() {
   
   return (
     <div className="App">
-      detail
+      {true && (
+               <div style={{textAlign: 'center'}}>
+                   {/* <h2> title </h2>
+                   <span> sub title </span>   */}
+
+                   <div align="center"> 
+                   <div align="left"><Button href="/"><Image alt='<-' src='/logo192.png' /* onClick={() => window.location.assign('/')} */></Image></Button></div>
+                    {/* <Table striped bordered hover> */}
+                    <Table>
+                      <thead>
+                        <tr align="center">
+                          <th colSpan="2"><h1>Title</h1></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr align="center">
+                          <td colSpan="2"><Image alt='' src='/logo512.png'></Image></td>
+                        </tr>
+                        <tr>
+                          <td align="right"><span><b>right</b></span></td>
+                          <td align="left"><span>left</span></td>
+                        </tr>
+                        <tr>
+                        <td align="right"><span><b>right</b></span></td>
+                          <td align="left"><span>left</span></td>
+                        </tr>
+                        <tr>
+                        <td align="right"><span><b>right</b></span></td>
+                          <td align="left"><span>left</span></td>
+                        </tr>
+                      </tbody>
+                    </Table>
+                   
+                      {/* <Row> 
+                          <Col md={2} />
+                          <Col md={2} style={{textAlign: "right"}}><h4>right</h4></Col>  
+                          <Col md={1} /> 
+                          <Col md={5} style={{textAlign: "left"}}><span>left</span></Col>                       
+                      </Row>
+                      <Row> 
+                        <Col style={{textAlign: "left"}}><Image alt='<-' src='/logo192.png'></Image></Col>    
+                      </Row>
+                      <Row> 
+                        <Col style={{textAlign: "center"}}><h1>Title</h1></Col>    
+                      </Row>
+                      <Row> 
+                        <Col style={{textAlign: "center"}}><Image alt='' src='/logo512.png'></Image></Col>    
+                      </Row>
+                      <Row> 
+                        <Col md={5} style={{textAlign: "ritght"}}>Right</Col>    
+                        <Col md={5} style={{textAlign: "left"}}><span>Left</span></Col>
+                      </Row>
+                      <Row>
+                        <Col style={{
+                          backgroundColor: 'red',
+                        }}>
+                          Sample First Col
+                        </Col>
+                          <Col style={{
+                            backgroundColor: 'yellow',
+                          }}>
+                            Sample Second Col
+                        </Col>
+                          <Col style={{
+                            backgroundColor: 'green',
+                          }}>
+                            Sample Third Col
+                        </Col>
+                      </Row> */}
+                   
+
+                   </div>
+                </div>   
+           )}
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,38 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Table, Container, Col, Row, Image } from 'react-bootstrap';
+import {useHistory} from 'react-router-dom'
+
+const data = {
+  "itemList":[
+    {
+      "id":1,
+      "title":"at",
+      "price":100,
+      "imageFile":"logo192.png"
+    },
+    {
+      "id":2,
+      "title":"bt",
+      "price":200,
+      "imageFile":"logo512.png"
+    },
+    {
+      "id":3,
+      "title":"ct",
+      "price":300,
+      "imageFile":"logo192.png"
+    }
+  ]
+}
+
+
 
 function Main() {
   
-  const [List, setList] = useState();
+  const history = useHistory();
+  const [list, setList] = useState([]);
+  const [newList, setNewList] = useState([]);
 
   const getList = () => {
     axios.get(`/`, {
@@ -13,23 +42,98 @@ function Main() {
     })
     .then( response => {
       console.log(response);
-      setList(response);
+      // setList(response);
+      // setList(data.itemList)
     })
     .catch(error => {
       console.log(error);
     })
     .then(() => {
       // always executed
+    }).finally(() => {
+      
     });
   }
 
+  const getDetail = (id) => {
+    // const chartDetail = await getChartDetail(id);
+    // console.log('chartDetail', chartList.chart);
+    if(id !== undefined) {
+      history.push('/detail/' + id);
+    }
+  }
+
   useEffect(() => {
-    getList();
+    // getList();
+    const newLine = [];
+      while(data.itemList.length > 0) {
+        const odd = data.itemList.shift() || undefined;
+        const even = data.itemList.shift() || undefined;
+        const line = [odd, even]
+        console.log(line);
+        newLine.push(line);
+      }
+      setNewList(newLine);
   }, [])
   
   return (
     <div className="App">
-      main
+      <Table align="center">
+        <thead>
+          <tr>
+            <th colSpan="4"><h1>Title</h1></th>
+          </tr>
+        </thead>
+        <tbody>
+          {newList && newList.length > 0 && newList.map((items, index) => {
+            // console.log(items.length, index, items[0], items[1]);
+            return (
+              <tr key={index}>
+                <td onClick={() => getDetail(items[0].id)}>
+                  <Table align="center">
+                    <tbody>
+                      <tr>{items[0].id}</tr>
+                      <tr>{items[0].title}</tr>
+                      <tr>{items[0].price}</tr>
+                      <tr><Image alt='' src={"/" + items[0].imageFile}></Image></tr>
+                    </tbody>
+                  </Table>
+                </td>
+                {items[1] && (
+                  <td onClick={() => getDetail(items[1].id)}>
+                    <Table align="center">
+                    <tbody>
+                        <tr>{items[1].id}</tr>
+                        <tr>{items[1].title}</tr>
+                        <tr>{items[1].price}</tr>
+                        <tr><Image alt='' src={"/" + items[1].imageFile}></Image></tr>
+                      </tbody>
+                    </Table>
+                  </td>
+                )
+
+                }
+              </tr>)
+          })}
+          {/* <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr> */}
+        </tbody>
+      </Table>
     </div>
   );
 }
